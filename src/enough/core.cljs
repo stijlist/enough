6932856d0262cfafed42d4d8535fe321126edc3e
Bert Muthalaly <bert@somethingdoneright.net>
2016-06-07 02:50:20 -0400

    Clean up debugging code, assert validity of :name in params to update

--- src/enough/core.cljs
+++ src/enough/core.cljs
@@ -6,30 +6,26 @@
 
 (enable-console-print!)
 (def init-data
-  {:parameters [[:parameters/by-name "Salary"]]
-   :parameters/by-name {"Salary" {:name "Salary" :value 1 :editing? false}}})
+  {:parameters [{:name "Salary" :value 1 :editing? false}]})
 
 (defmulti read om/dispatch)
 
 (defn get-normalized-toplevel-key [state key]
   (let [s @state]
-    (prn "state in get-n-t-k" s)
     (into [] (map #(get-in s %) (get s key)))))
 
 (defmethod read :parameters
   [{:keys [state]} key params]
-  (prn "calling read with key:" key "state:" @state "and params" params)
-  (prn "returning:" (get-normalized-toplevel-key state key))
   {:value (get-normalized-toplevel-key state key)})
 
 (defmulti mutate om/dispatch)
 
 (defmethod mutate 'parameters/update
   [{:keys [state]} key {:keys [name] :as params}]
-  (prn "params to mutation" params)
+  {:pre [(string? name)]}
   {:action
    (fn []
-     (swap! state update-in [:parameters/by-name name] (constantly params)))})
+     (swap! state update-in [:parameters/by-name name] (fn [old] (merge old params))))})
 
 (defui Parameter
   static om/Ident
@@ -44,7 +40,8 @@
      (html 
        [:div nil 
         (str (om/props this))
-        [:button {:onClick #(om/transact! this '[(parameters/update {:name "Salary" :value 2 :editing? false})])} "Set value to 2"]]))))
+        [:button {:onClick #(om/transact! this '[(parameters/update {:name "Salary" :editing? true})])} "Set editing to true"]
+        [:button {:onClick #(om/transact! this '[(parameters/update {:name "Salary" :value 2})])} "Set value to 2"]]))))
 
 (def parameter (om/factory Parameter {:keyfn :name}))
 
@@ -52,29 +49,13 @@
   static om/IQuery
   (query [this]
     (let [subquery (om/get-query Parameter)]
-      ;; (assert (= subquery [:name :value :editing?]))
-      ;; mysteriously, substituting om/get-query Parameter here doesn't yield
-      ;; the same result, even though the subqueries are equal
-      ;; instead, om/props of the Parameter starts returning the ident instead
-      ;; one explanation would be that the subquery is changing, but it's not -
-      ;; (prn "subqueries equal" keeps printing "true"
-      ;; perhaps I could assert that the subqueries were equal to make sure
-      ;; I'm not just misreading the test log
-      ;; (prn "subqueries equal" (= subquery '[:name :value :editing?]))
-      ;; `[{:parameters ~subquery}]))
       `[{:parameters ~subquery}]))
   Object
   (render [this]
-    (prn "rendering Root" (om/props this))
     (html 
       [:div nil (map parameter (:parameters (om/props this)))])))
 
 (def parser (om/parser {:read read :mutate mutate}))
 (def reconciler (om/reconciler {:state init-data :parser parser}))
-(add-watch (om/app-state reconciler) :parameters-by-name-contains-salary
-  (fn [k ref old new] 
-    (let [pred #(-> % :parameters/by-name (contains? "Salary"))]
-      (assert (pred old) (str "Old is " old))
-      (assert (pred new) (str "New is " new)))))
 
 (om/add-root! reconciler Root (dom/getElement "app"))
