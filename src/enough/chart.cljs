9da5f4ecef057686221ca631226efa11523f45a5
Bert Muthalaly <bert@somethingdoneright.net>
2016-06-12 23:36:01 -0400

    Display income growth on graph, render final year of simulation

--- src/enough/chart.cljs
+++ src/enough/chart.cljs
@@ -1,9 +1,6 @@
 (ns enough.chart
   (:require [sablono.core :refer-macros [html]]))
 
-;; now, for each point, we want to overlay the year's expenses onto the year's
-;; income. so the return values of this have to be separate lists of data
-;; points - the income growth, balance growth, and expenses
 (defn years-til-retirement
   [{:keys [salary expenses rate-of-return cutoff]}]
   {:pre [(number? salary) 
@@ -17,15 +14,12 @@
           done? (or 
                   (>= (count years) cutoff)
                   (>= growth expenses)
-                  (< balance 0))]
-      (if done?
-        years
-        (recur 
-          (conj years 
-            {:balance new-balance 
-             :expenses expenses 
-             :income-growth growth 
-             :income salary}))))))
+                  (< balance 0))
+          next (conj years {:balance new-balance 
+                            :expenses expenses 
+                            :income-growth growth 
+                            :income salary})]
+      (if done? next (recur next)))))
 
 (defn linear-scale [[domain-start domain-end]
                     [range-start range-end]]
@@ -42,6 +36,7 @@
 (defn bar-chart [data {:keys [width height]}]
   (let [balances (map :balance data) 
         expenses (map (juxt :balance :expenses) data)
+        income-growth (map (juxt :balance :income-growth) data)
         true-height 500
         y-scale (linear-scale [0 1000000] [0 true-height])
         bar-width 40
@@ -73,4 +68,16 @@
                 :width (dec bar-width)}]
               [:text text-offsets (thousands->k d)]])
            expenses)
+         (map-indexed
+           (fn [i [balance-offset d]]
+             [:g {:transform
+                  (translate (* i bar-width) (- (- (y-scale balance-offset) (y-scale d))))}
+              [:rect 
+               {:fill "lightgreen"
+                :y (- true-height (y-scale d))
+                :height (y-scale d)
+                :width (dec bar-width)}]
+              (when (> (y-scale d) 10)
+                [:text text-offsets (thousands->k d)])])
+           income-growth)
          ]])))
