f996cceeff08641fcafa848a09703548595e6666
Bert Muthalaly <bert@somethingdoneright.net>
2016-08-06 02:14:54 -0500

    Experimenting with optimizing chart rendering

--- src/enough/chart.cljs
+++ src/enough/chart.cljs
@@ -1,41 +1,46 @@
 (ns enough.chart
   (:require [sablono.core :refer-macros [html]]
+            [om.dom :as dom]
             [om.next :as om :refer-macros [defui]]))
 
 (defn multimap [kvs]
   (let [assoc-val-as-set 
         (fn [m [k v]]
-          (if (contains? m k) 
-            (assoc m k (conj (get m k) v))
-            (assoc m k #{v})))]
+          (assoc m k (if-not (contains? m k) #{v} (conj (get m k) v))))]
     (reduce assoc-val-as-set {} kvs)))
 
 (defn life-events-by-year [life-events]
   (let [get-year-ev-pairs (fn [e] (for [k (-> e :costs-per-year keys)] [k e]))]
     (multimap (mapcat get-year-ev-pairs life-events))))
 
-(defrecord Year [^number balance ^number income-growth ^number expenses expense-breakdown])
+(def year->life-events (memoize life-events-by-year))
+
+(defrecord Year [^number index ^number balance ^number income-growth ^number expenses])
 
 (defn years-til-retirement
-  [{:keys [salary expenses rate-of-return initial-savings cutoff life-events]}]
+  [{:keys [^number salary ^number expenses ^number rate-of-return ^number initial-savings ^number cutoff life-events]}]
   {:pre [(number? salary)
          (number? expenses)
          (number? rate-of-return)
          (number? initial-savings)
          (not (nil? cutoff))]}
-  (let [year->life-events (life-events-by-year life-events)]
-    (loop [years []]
-      (let [balance (or (:balance (peek years)) initial-savings)
-            growth (* balance rate-of-return)
-            this-year (count years)
-            expense-breakdown (get year->life-events this-year)
-            new-balance (- (+ balance salary growth) expenses)
-            done? (or 
-                    (>= this-year cutoff)
-                    (>= growth expenses)
-                    (< balance 0))
-            next (conj years (Year. new-balance growth expenses expense-breakdown))]
-        (if done? (do (prn this-year) next) (recur next))))))
+  ;; year->life-events only needs to be recomputed when life-events change, pull out of ytr
+  (loop [years (transient []) balance initial-savings]
+    (let [growth (* balance rate-of-return)
+          this-year (count years)
+          expense-breakdown (get (year->life-events life-events) this-year)
+          get-costs-this-year (map #(get-in % [:costs-per-year this-year]))
+          variable-costs (transduce get-costs-this-year + expense-breakdown)
+          total-costs (+ expenses variable-costs)
+          new-balance (- (+ balance salary growth) total-costs)
+          done? (or 
+                  (>= this-year cutoff)
+                  (>= growth expenses)
+                  (< balance 0))
+          next (conj! years (Year. this-year new-balance growth expenses))]
+      (if done?
+        {:max-bar-value (+ total-costs new-balance) :data (persistent! next) :num-years this-year}
+        (recur next new-balance)))))
 
 (defn linear-scale [[domain-start domain-end]
                     [range-start range-end]]
@@ -51,27 +56,33 @@
 
 (defn render-balance 
   [{:keys [bar-width true-height y-scale text-offsets]}]
-  (fn [i d]
-    [:g {:transform (translate (* i bar-width) 0)}
-     [:rect 
-      {:fill "steelblue"
-       :y (- true-height (y-scale d)) 
-       :height (y-scale d) 
-       :width (dec bar-width)}]
-     [:text text-offsets (thousands->k d)]]))
+  (fn [{:keys [index balance]}]
+    (let [height (y-scale balance)]
+      (dom/g
+        #js {:transform (translate (* index bar-width) 0)}
+        (dom/rect
+          #js {:fill "steelblue"
+               :y (- true-height height) 
+               :height height 
+               :width (dec bar-width)})
+        (dom/text
+          (clj->js text-offsets) (thousands->k balance))))))
 
 (defn render-income-growth
   [{:keys [bar-width true-height y-scale text-offsets]}]
-  (fn [i [balance-offset d]]
-    [:g {:transform
-         (translate (* i bar-width) (- (- (y-scale balance-offset) (y-scale d))))}
-     [:rect 
-      {:fill "mediumaquamarine"
-       :y (- true-height (y-scale d))
-       :height (y-scale d)
-       :width (dec bar-width)}]
-     (when (> (y-scale d) 10)
-       [:text text-offsets (thousands->k d)])]))
+  (fn [{:keys [index balance income-growth]}]
+    (let [scaled-bal (y-scale balance)
+          scaled-g (y-scale income-growth)]
+      (dom/g
+        #js {:transform
+             (translate (* index bar-width) (- (- scaled-bal scaled-g)))}
+        (dom/rect
+          #js {:fill "mediumaquamarine"
+               :y (- true-height scaled-g)
+               :height scaled-g
+               :width (dec bar-width)}
+          (when (> scaled-g 10)
+            (dom/text (clj->js text-offsets) (thousands->k income-growth))))))))
 
 (defn points [& xys]
   (map (fn [[x y]] (str x "," y)) xys))
@@ -79,55 +90,53 @@
 (defui ExpensesSegment
   static om/Ident
   (ident [this props]
-    [:expenses-segments/by-index (:i props)])
+    [:expenses-segments/by-index (:index props)])
   Object
   (render [this]
-    (let [{:keys [i balance expenses expense-breakdown focused
-                  true-height bar-width y-scale text-offsets]} (om/props this)
+    (let [{:keys [index balance expenses focused]} (om/props this)
           {:keys [mouseover?]} (om/get-state this)
-          variable-costs (transduce (map #(get-in % [:costs-per-year i])) + expense-breakdown)
-          display-popover! (om/get-computed this :display-popover!)
-          d (+ expenses variable-costs)]
-      (html
-        [:g {:transform 
+          {:keys [true-height bar-width y-scale text-offsets display-popover!]} (om/get-computed this)
+          i index
+          d expenses]
+      (dom/g
+        #js {:transform 
              (translate (* i bar-width) (- (y-scale balance)))
              :onMouseOver #(om/update-state! this assoc :mouseover? true)
              :onMouseLeave #(om/update-state! this assoc :mouseover? false)
-             :onClick #(display-popover! 
-                         {:ident (om/get-ident this) :position {:top (- true-height (+ (y-scale balance) (y-scale d))) :left (* i bar-width)} :message [:span "test"]})}
-         [:rect
-          {:fill (if mouseover? "lightblue" "lightcoral")
-           :y (- true-height (y-scale d))
-           :height (y-scale d)
-           :width (dec bar-width)}]
-         [:text text-offsets (thousands->k d)]]))))
+             :onClick 
+             #(display-popover!
+               {:ident (om/get-ident this)
+                :position {:top (- true-height (+ (y-scale balance) (y-scale d)))
+                           :left (* i bar-width)}
+                           :message [:span "test"]})}
+        (dom/rect
+          #js {:fill (if mouseover? "lightblue" "lightcoral")
+               :y (- true-height (y-scale d))
+               :height (y-scale d)
+               :width (dec bar-width)})
+        (dom/text (clj->js text-offsets) (thousands->k d))))))
 
-(def render-expenses (om/factory ExpensesSegment {:keyfn #(-> % (get :i) (str "-expense"))}))
+(defn render-year [year])
 
-(defn savings-chart [data {:keys [display-popover!] :as opts}]
-  (let [balances (map :balance data) 
-        expenses (map #(select-keys % [:balance :expenses :expense-breakdown]) data)
-        income-growth (map (juxt :balance :income-growth) data)
-        pixels-per-thousand 0.5
-        max-bar-value (reduce + (-> (last data) (select-keys [:balance :expenses :income-growth]) vals))
+(def render-expenses (om/factory ExpensesSegment {:keyfn #(-> % (get :index) (str "-expense"))}))
+
+(defn savings-chart [{:keys [max-bar-value data num-years]} {:keys [display-popover!] :as opts}]
+  (let [pixels-per-thousand 0.5
         max-bar-height (* pixels-per-thousand (/ max-bar-value 1000))
         bar-width 40
         chart-opts
         {:true-height max-bar-height
          :y-scale (linear-scale [0 max-bar-value] [0 max-bar-height])
          :bar-width bar-width
-         :text-offsets {:x (+ 7 (/ bar-width 2)) :y (- max-bar-height 3) :dy "0.15em"}}]
-    (html
-      [:svg
-       {:class "chart"
-        :height (str max-bar-height "px")
-        :width (str (* bar-width (count data)) "px")}
-       (map-indexed (render-balance chart-opts) balances)
-       (map-indexed
-          (fn [i m]
-            (render-expenses (om/computed (assoc (merge m chart-opts) :i i) {:display-popover! display-popover!})))
-          expenses)
-       (map-indexed (render-income-growth chart-opts) income-growth)])))
+         :text-offsets {:x (+ 7 (/ bar-width 2)) :y (- max-bar-height 3) :dy "0.15em"}
+         :display-popover! display-popover!}]
+    (dom/svg
+      #js {:className "chart"
+           :height (str max-bar-height "px")
+           :width (str (* bar-width (count data)) "px")}
+      (map (render-balance chart-opts) data)
+      (map #(render-expenses (om/computed % chart-opts)) data)
+      (map (render-income-growth chart-opts) data))))
 
 (defui SavingsChart
   static om/IQuery
@@ -136,7 +145,7 @@
   (render [this]
     (let [props (om/props this)]
       (time
-        (-> props 
-          years-til-retirement 
-          (savings-chart 
-            {:display-popover! #(om/transact! this `[(popovers/show ~%) :popovers])}))))))
+      (-> props 
+        years-til-retirement 
+        (savings-chart 
+          {:display-popover! #(om/transact! this `[(popovers/show ~%) :popovers])}))))))
