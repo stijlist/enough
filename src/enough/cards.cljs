64562d8a59a0c744ad54c12f5167b9f36b07c685
Bert Muthalaly <bert@somethingdoneright.net>
2016-08-13 02:13:27 +0400

    Extract card functionality into reusable fn

--- src/enough/cards.cljs
+++ src/enough/cards.cljs
@@ -1,5 +1,6 @@
 (ns enough.cards
   (:require 
+    [enough.cards.macros :refer [atom-named]]
     [enough.ui :refer [LifeEventForm]]
     [clojure.set :as set]
     [goog.dom :refer [append createElement getElement]]
@@ -7,34 +8,63 @@
 
 (enable-console-print!)
 
-(let [_ (defonce event-form-state (atom {:creating? false}))
-      watch (getElement "watch-root")
-      _ (aset watch "innerHTML" (str "<div>" @event-form-state "</div>"))
-      read
-      (fn [{:keys [state] :as env} key params]
-        {:value (get @state key)})
-      mutate
-      (fn [{:keys [state] :as env} key params]
-        {:action
-         (condp = key
-           'events/new #(swap! state assoc :creating? true)
-           'events/cancel #(swap! state assoc :creating? false)
-           'events/save 
-           #(let [add-event
-                  (fn [state]
-                    (-> state
-                      (update :life-events conj params)
-                      (assoc :creating? false)))]
-               (swap! state add-event)))})
-      parser (om/parser {:read read :mutate mutate})]
-  (add-watch event-form-state :card
-    (fn [k ref old new] 
-      (let [old-keys (set (keys old)) new-keys (set (keys new))]
-        (prn old new)
-        (aset watch "innerHTML" (str "<div>" new "</div>"))
-        (prn "added:" (set/difference new-keys old-keys))
-        (prn "changed:" (select-keys new (set/intersection old-keys new-keys))))))
-  (om/add-root!
-    (om/reconciler {:state event-form-state :parser parser})
-    LifeEventForm
-    (getElement "test-root")))
+(def atoms (atom {}))
+
+(defn card [& {:keys [name component init-state read mutate]}]
+  (assert name "name must be provided")
+  (when-not (getElement "card-roots")
+    (append
+      (.-body js/document)
+      (doto 
+        (createElement "div")
+        (aset "id" "card-roots"))))
+  (when-not (getElement name)
+    (append
+      (getElement "card-roots")
+      (doto
+        (createElement "div")
+        (aset "id" name))))
+  (when-not (getElement (str name "-watch"))
+    (append
+      (getElement "card-roots")
+      (doto
+        (createElement "div")
+        (aset "id" (str name "-watch")))))
+  (let [state
+        (or 
+          (get atoms name)
+          (let [new (atom init-state)]
+            (swap! atoms assoc name new)
+            new))
+        root-div (getElement name)
+        watch-div (getElement (str name "-watch"))
+        update-watch #(aset watch-div "innerHTML" (str "<div>" % "</div>"))
+        parser (om/parser {:read read :mutate mutate})
+        reconciler (om/reconciler {:state state :parser parser})]
+    (update-watch @state)
+    (add-watch state :card 
+      (fn [k ref old new]
+        (let [old-keys (set (keys old)) new-keys (set (keys new))]
+          (update-watch new)
+          (prn "added:" (set/difference new-keys old-keys))
+          (prn "changed:" (select-keys new (set/intersection old-keys new-keys))))))
+    (om/add-root! reconciler component root-div)))
+
+(card 
+  :name "event-form"
+  :component LifeEventForm
+  :init-state {:creating? false}
+  :read  (fn [{:keys [state] :as env} key params] {:value (get @state key)})
+  :mutate 
+  (fn [{:keys [state] :as env} key params]
+    {:action
+     (condp = key
+       'events/new #(swap! state assoc :creating? true)
+       'events/cancel #(swap! state assoc :creating? false)
+       'events/save 
+       #(let [add-event
+              (fn [state]
+                (-> state
+                  (update :life-events conj params)
+                  (assoc :creating? false)))]
+           (swap! state add-event)))}))
