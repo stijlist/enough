000287b096a5b41d21ff7aee244b99ae5ef99049
Bert Muthalaly <bert@somethingdoneright.net>
2016-08-13 01:10:23 +0400

    State-preserving life event form in cards.html

--- src/enough/cards.cljs
+++ src/enough/cards.cljs
@@ -1,12 +1,40 @@
 (ns enough.cards
   (:require 
-    [goog.dom]
+    [enough.ui :refer [LifeEventForm]]
+    [clojure.set :as set]
+    [goog.dom :refer [append createElement getElement]]
     [om.next :as om]))
 
 (enable-console-print!)
-(om/add-root!
-  (om/reconciler {:state {} :parser (om/parser {:read #() :mutate #()})})
-  (om/ui
-    Object
-    (render [this] (prn "root mounted")))
-  (goog.dom/getElement "test-root"))
+
+(let [_ (defonce event-form-state (atom {:creating? false}))
+      watch (getElement "watch-root")
+      _ (aset watch "innerHTML" (str "<div>" @event-form-state "</div>"))
+      read
+      (fn [{:keys [state] :as env} key params]
+        {:value (get @state key)})
+      mutate
+      (fn [{:keys [state] :as env} key params]
+        {:action
+         (condp = key
+           'events/new #(swap! state assoc :creating? true)
+           'events/cancel #(swap! state assoc :creating? false)
+           'events/save 
+           #(let [add-event
+                  (fn [state]
+                    (-> state
+                      (update :life-events conj params)
+                      (assoc :creating? false)))]
+               (swap! state add-event)))})
+      parser (om/parser {:read read :mutate mutate})]
+  (add-watch event-form-state :card
+    (fn [k ref old new] 
+      (let [old-keys (set (keys old)) new-keys (set (keys new))]
+        (prn old new)
+        (aset watch "innerHTML" (str "<div>" new "</div>"))
+        (prn "added:" (set/difference new-keys old-keys))
+        (prn "changed:" (select-keys new (set/intersection old-keys new-keys))))))
+  (om/add-root!
+    (om/reconciler {:state event-form-state :parser parser})
+    LifeEventForm
+    (getElement "test-root")))
