26502107564c0462b5a07dad247557f76e4349e8
Bert Muthalaly <bert@somethingdoneright.net>
2016-12-31 02:25:00 -0600

    Minimal repro case for ui (will modify data next)

--- src/enough/root.cljs
+++ src/enough/root.cljs
@@ -1,11 +1,40 @@
 (ns enough.root
   (:require
     [enough.data]
-    [enough.ui :refer [LifeEvent LifeEventForm]]
-    [enough.ui.chart :refer [SavingsChart]]
     [om.next :as om :refer [defui]]
     [om.dom :as dom]
     [goog.dom]))
+
+(def static-txn {:name "Test"})
+
+(defui LifeEvent
+  static om/Ident
+  (ident [this {:keys [name]}]
+    [:life-events/by-name name])
+  static om/IQuery
+  (query [this]
+    '[:name :costs-per-year :cost :constant?])
+  Object
+  (render [this]))
+
+(defui LifeEventForm
+  static om/IQuery
+  (query [this]
+    [:creating?])
+  Object
+  (render [this]
+    (let []
+      (dom/div nil
+        (dom/button #js {:onClick #(om/transact! this `[(events/save ~static-txn) :life-events])} "Doesn't repro")
+        (dom/button #js {:onClick #(om/transact! this `[(events/save ~static-txn) :life-events :chart])} "Repro")))))
+
+(defui SavingsChart
+  static om/IQuery
+  (query [this] '[:chart])
+  Object
+  (render [this]
+    (let [{:keys [chart life-events] :as props} (om/props this)]
+      (assert (not (nil? life-events)) "Life events cannot be nil"))))
 
 (def render-chart (om/factory SavingsChart))
 (def life-event-form (om/factory LifeEventForm))
@@ -19,7 +48,7 @@
   Object
   (render [this]
     (let [{:keys [life-events] :as props} (om/props this)]
-      (prn props)
+      (prn "props are" props)
       (dom/div 
         #js {:style #js {:margin "0 auto" :max-width 600}}
         (dom/div nil
